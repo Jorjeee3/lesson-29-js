f9a63a03cf4d01ca72568965a2079a1db495fc97
Jorjeee3 <look7at7george@gmail.com>
2021-03-10 18:42:17 +0200

    change

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const linkItems = document.querySelectorAll(".item-link");
-const getFocus = document.querySelector(".active");
-
-for (item of linkItems) {
-    linkItems.addEventListener("click", focusItem)
-}
 
 function focusItem() {
-    if (linkItems.classList.contains("active")) {
-        linkItems.classList.remove("active");
-    } else {
-        linkItems.classList.add("active");
+
+
+    for (linkItem of linkItems) {
+
+        if (linkItem.classList.contains("active")) {
+            linkItem.classList.remove("active");
+        }
+        
     }
+
+    this.classList.add("active")
+
+
 }
+
+for (linkItem of linkItems) {
+    linkItem.addEventListener("click", focusItem)
+}
+
